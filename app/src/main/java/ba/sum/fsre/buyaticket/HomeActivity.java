76536a2cf7c0191aa47f130d52a4a96f819ed114
AntonioPavkovic <antoniopavkovic999@gmail.com>
2024-01-22 17:15:33 +0100

    fixed minor issues

--- app/src/main/java/ba/sum/fsre/buyaticket/HomeActivity.java
+++ app/src/main/java/ba/sum/fsre/buyaticket/HomeActivity.java
@@ -34,7 +34,6 @@ public class HomeActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private FirebaseRecyclerAdapter<CardModel, CardViewHolder> cardAdapterHolder;
-    private LinearLayout sortingButtonsLayout;
 
 
 
